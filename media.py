64c2acd032dd7c6080be873ac81bf3031892b80e
Marcin Wisniowski <hollowlink@gmail.com>
2015-04-27 00:54:28 -0400

    Initial commit

--- media.py
+++ media.py
@@ -1,18 +1,17 @@
 import os
 import requests
 
-# loads environmental variables from a file,
-# default: '$PWD/.env'
+# Loads environmental variables from a file,
+# by default it will load all variables from '.env'
 from env_tools import apply_env; apply_env()
 
-# TMDb API requires authentication with an api key
-# provide the key in an enviromnetal variable
+# TMDb API configuration
 API_VERSION = '3'
 API_ROOT_URL = 'https://api.themoviedb.org/{version}'.format(version=API_VERSION)
 API_KEY = os.environ['TMDB_API_KEY']
 
-# To build image URL from TMDb three elements are required: base url, size, and file path.
-# Base url and a list of sizes is returned from calling TMDb API /configuration method
+# To build image URL from TMDb three elements are required: base URL, size, and file path.
+# Base URL and a list of sizes is returned from calling TMDb API /configuration method
 # http://docs.themoviedb.apiary.io/#reference/configuration
 url = API_ROOT_URL + '/configuration'
 request = requests.get(url, params={'api_key': API_KEY})
@@ -22,12 +21,12 @@ IMAGE_BASE_URL += request.json()['images']['poster_sizes'][4]
 
 class movie(object):
     """Movie object with three properties: a writable title, poster image URL,
-    and YouTube! trailer URL.
+    and trailer youtube id.
 
     Attributes:
         title: A string.
         poster_image_url: String, the URL for the movie's poster image
-        trailer_youtube_id: String, YouTube! video is, used to fetch trailer.
+        trailer_youtube_id: String, youtube trailer video id, used to fetch trailer.
 
     To use:
         >>> the_matrix = movie(683)
@@ -40,6 +39,8 @@ class movie(object):
     """
 
     def __init__(self, id):
+        # Request a movie from TMBb API, with extended attributes
+        # http://docs.themoviedb.apiary.io/#reference/movies
         url = API_ROOT_URL + '/movie/{id}'.format(id=str(id))
         request = requests.get(url, params={'api_key': API_KEY,
             'append_to_response': 'videos'})
@@ -48,5 +49,4 @@ class movie(object):
         self.title = movie['title']
         self.poster_image_url = IMAGE_BASE_URL + movie['poster_path']
         self.trailer_youtube_id = movie['videos']['results'][0]['key']
-
 
